6d6d2301eacdb5354871b21b337030ce63d57aa8
Stephane Moreau <st.moreau@outlook.com.gr>
2017-02-05 01:21:05 +0000

    first working draft

--- Silly song/ViewController.swift
+++ Silly song/ViewController.swift
@@ -8,11 +8,47 @@
 
 import UIKit
 
+func shortNameFromName(name: String) -> String {
+    let vowelCharacterset = CharacterSet.init(charactersIn: "aeiou")
+    var name = name.lowercased().folding(options: String.CompareOptions.diacriticInsensitive, locale: NSLocale.current)
+    let range = name.startIndex..<name.index(after: name.startIndex)
+    while ((name.rangeOfCharacter(from: vowelCharacterset, options: String.CompareOptions.diacriticInsensitive, range: range)) == nil) {
+        name = name.substring(from: name.index(after: name.startIndex))
+    }
+    return name
+}
+
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joined(separator: "\n")
+
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    let sillySong = lyricsTemplate.replacingOccurrences(of: "<FULL_NAME>", with: fullName).replacingOccurrences(of: "<SHORT_NAME>", with: shortNameFromName(name: fullName))
+    return sillySong
+}
+
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
+
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +57,15 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = ""
+        lyricsView.text = ""
+    }
+    @IBAction func displayLyrics(_ sender: Any) {
+        if nameField.text != "" {
+            let lyrics = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: nameField.text!)
+            lyricsView.text = lyrics
+        }
+    }
 }
 
